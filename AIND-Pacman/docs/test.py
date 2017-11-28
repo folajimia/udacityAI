a7eae5e18e42dda625012ca39b1a879dbad57b75
Jimi Adekoya <folajimia@gmail.com>
2017-11-28 19:07:57 +0100

    Pacman serach exe

---
+++ AIND-Pacman/docs/test.py
@@ -0,0 +1,62 @@
+def tree_search(problem, frontier):
+    """Search through the successors of a problem to find a goal.
+    The argument frontier should be an empty queue.
+    Don't worry about repeated paths to a state. [Figure 3.7]"""
+    frontier.append(Node(problem.initial))
+    while frontier:
+        node = frontier.pop()
+        if problem.goal_test(node.state):
+            return node
+        frontier.extend(node.expand(problem))
+    return None
+
+
+def graph_search(problem, frontier):
+    """Search through the successors of a problem to find a goal.
+    The argument frontier should be an empty queue.
+    If two paths reach a state, only use the first one. [Figure 3.7]"""
+    frontier.append(Node(problem.initial))
+    explored = set()
+    while frontier:
+        node = frontier.pop()
+        if problem.goal_test(node.state):
+            return node
+        explored.add(node.state)
+        frontier.extend(child for child in node.expand(problem)
+                        if child.state not in explored and
+                        child not in frontier)
+    return None
+
+
+def breadth_first_tree_search(problem):
+    """Search the shallowest nodes in the search tree first."""
+    return tree_search(problem, FIFOQueue())
+
+
+def depth_first_tree_search(problem):
+    """Search the deepest nodes in the search tree first."""
+    return tree_search(problem, Stack())
+
+
+def depth_first_graph_search(problem):
+    """Search the deepest nodes in the search tree first."""
+    return graph_search(problem, Stack())
+
+
+def breadth_first_search(problem):
+    """[Figure 3.11]"""
+    node = Node(problem.initial)
+    if problem.goal_test(node.state):
+        return node
+    frontier = FIFOQueue()
+    frontier.append(node)
+    explored = set()
+    while frontier:
+        node = frontier.pop()
+        explored.add(node.state)
+        for child in node.expand(problem):
+            if child.state not in explored and child not in frontier:
+                if problem.goal_test(child.state):
+                    return child
+                frontier.append(child)
+    return None
